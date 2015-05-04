25df3fd67edded32b5027cfcbfa16b6bae7cdd93
meppe <meppe@iiia.csic.es>
2015-05-04 10:20:33 -0700

    no message

--- Amalgamation/settings.py
+++ Amalgamation/settings.py
@@ -6,7 +6,7 @@ inputFile = "examples/minimal.casl"
 inputSpaceNames = ["S1","S2"]
 
 ###### The number of models to be generated (0 for all models)
-numModels = 1
+numModels = 20
 
 ###### The minimal number of iterations for generalization
 minIterationsGeneralize = 1
@@ -15,7 +15,7 @@ minIterationsGeneralize = 1
 maxIterationsGeneralize = 20
 
 ###### Percentage of blend value below the currently highest value found so far that we want to keep in the results. 0 means that we only keep the blends with the highest value found so far, 100 means to consider all blends.
-blendValuePercentageBelowHighestValueToKeep = 0
+blendValuePercentageBelowHighestValueToKeep = 10
 
 ###### Time limit for eprover and darwin consistency check in seconds CPU time
 eproverTimeLimit = 4
@@ -37,10 +37,10 @@ hetsExe = 'hets'
 # inputFile = "examples/LPNMR/cadenceBlendFusionTest.casl"
 # inputSpaceNames = ["PhrygianCadence","PerfectCadence"]
 
-# inputFile = "examples/LPNMR/naturalsAndLists.casl"
-# inputSpaceNames = ["NatSuc","List"]
+inputFile = "examples/LPNMR/naturalsAndLists.casl"
+inputSpaceNames = ["NatSuc","List"]
 
-inputFile = "examples/metaphorTest.casl"
-inputSpaceNames = ["EvilCreature","Fire"]
+# inputFile = "examples/metaphorTest.casl"
+# inputSpaceNames = ["EvilCreature","Fire"]
 
 
