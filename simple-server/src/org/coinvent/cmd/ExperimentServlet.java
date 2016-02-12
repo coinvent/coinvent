5f01018e43cf846d62fdc476cd658c8951d6ca45
Ewen Maclean <ewen@dhcp-91-118.inf.ed.ac.uk>
2016-02-12 13:54:27 +0000

    experiment files

--- simple-server/src/org/coinvent/cmd/ExperimentServlet.java
+++ simple-server/src/org/coinvent/cmd/ExperimentServlet.java
@@ -5,6 +5,7 @@ import org.coinvent.ProcessActiveTriple.ActiveType;
 import org.coinvent.HdtpRequests.HdtpRequest;
 import org.coinvent.HdtpRequests.ReadType;
 import org.coinvent.IServlet;
+import org.apache.commons.lang3.ArrayUtils;
 import org.coinvent.CoinventConfig;
 import org.coinvent.ProcessActiveTriple;
 import org.eclipse.jetty.io.BufferCache.CachedBuffer;
@@ -51,61 +52,126 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.net.InetAddress;
-/**
-* Calls HETS via HETS API.
-* 
-* Try: http://localhost:8300/static/blendui/index.html
-* 
-* <h2>API</h2>
-* 
-* <h3>Call HETS</h3>
-* Endpoint: /cmd/HETS<br>
-* 
-* Parameters:<br>
-*
-*  - action: full-theories returns output file from hets after doing an auto-dg  <br>
-             consistency-check returns result of consistency check with eprover via HETS <br>
-   - pid: pid of current HDTP process - uniquely defined file to be put on localhost url for HETS API
-*  - contents: the contents of the file to be sent to HETS 
-*  
-* Returns: JSON-format object<br>
-* <pre>
-* {
-* 	success:boolean,
-*  cargo: {
-*  	fulloutput: {text: string},
-*  	blendoutput: {text: string},
-*  	consistency: {text: string}
-*  	}
-* }
-* </pre>
-* 
-* @author ewen
-* 
-*/
+
 
 
 public class ExperimentServlet implements IServlet {
 
 		private InputFiles[] inputfile;
-		
+	    Random randomNum;
 		private void setupInputFiles() {
-			inputfile=new InputFiles[2];
-			inputfile[0] = new InputFiles("/web/static/experiment_files/1");
-			inputfile[1] = new InputFiles("/web/static/experiment_files/2");
+			
+			File l = FileUtils.getWorkingDirectory();
+			String path = l.getAbsolutePath();
+			inputfile=new InputFiles[10];
+			inputfile[0] = new InputFiles(path+"/web/static/experiment_files/0");
+			inputfile[1] = new InputFiles(path+"/web/static/experiment_files/1");
+			inputfile[2] = new InputFiles(path+"/web/static/experiment_files/2");
+			inputfile[3] = new InputFiles(path+"/web/static/experiment_files/3");
+			inputfile[4] = new InputFiles(path+"/web/static/experiment_files/4");
+			inputfile[5] = new InputFiles(path+"/web/static/experiment_files/5");
+			inputfile[6] = new InputFiles(path+"/web/static/experiment_files/6");
+			inputfile[7] = new InputFiles(path+"/web/static/experiment_files/7");
+			inputfile[8] = new InputFiles(path+"/web/static/experiment_files/8");
+			inputfile[9] = new InputFiles(path+"/web/static/experiment_files/9");
+			
+		}
+		
+		private int get_new_integer(Integer[] used,String s){
+			randomNum=new Random();
+			if (s == null)
+			{
+			    return randomNum.nextInt(10);
+			}
+			
+		    Integer[] stat_inps = new Integer[10];
+		    for (int i=0;i<10;i++){
+		    	stat_inps[i] = i;
+		    }
+			for (String n : s.split(" ")) {
+				  int num=0;
+				  try
+				  {
+				    num = Integer.parseInt(n);
+				  }
+				  catch (NumberFormatException e)
+				  {}
+				  finally
+				  {
+					  int ind=0;
+					  for (int i=0;i<stat_inps.length;i++)
+					  {
+						  if (stat_inps[i]==num)
+						  {
+							  ind = i;
+							  break;
+						  }
+					  }
+					  stat_inps = ArrayUtils.remove(stat_inps, ind);
+				  }
+			}
+			
+			
+		    int res = randomNum.nextInt(stat_inps.length);
+			
+			return stat_inps[res];
+			
 		}
 		
 		@Override
-		public void doPost(WebRequest webRequest) throws Exception {		
+		public void doPost(WebRequest webRequest) throws Exception {
+			randomNum = new Random();
 			setupInputFiles();
+			
+			if (webRequest.actionIs("example"))
+			{
+				File l = FileUtils.getWorkingDirectory();
+				String path = l.getAbsolutePath();
+				InputFiles i = new InputFiles(path+"/web/static/experiment_files/example");
+				try {
+					ArrayMap cargo = new ArrayMap(				
+							"stheorem", i.source_theorem				
+							);
+					
+						
+						cargo.put("slemma", i.source_lemma);
+						cargo.put("ttheorem", i.target_theorem);
+						cargo.put("tlemma", i.target_lemma);
+						cargo.put("stheory", i.source_theory);
+						cargo.put("ttheory", i.target_theory);
+					    cargo.put("result", "yes");
+					    cargo.put("file_id",i.file_id);
+					
+					JsonResponse jr = new JsonResponse(webRequest, cargo);
+					WebUtils2.sendJson(jr, webRequest);
+					
+				}
+			     catch (IOException e) {
+					e.printStackTrace();
+				 
+				}
+				
+			}
+			
+			
 			if (webRequest.actionIs("get"))
 			{
-				String numstr = (webRequest.get("current"));
-				int num =Integer.parseInt(numstr.trim());
+				
+				
+				
+				//Put file ids in here - sending them too....
+				//under key "used"
+				String numstr = (webRequest.get("used"));
+				//int num =Integer.parseInt(numstr.trim());
+				Integer[] used = new Integer[5];
+				int num = get_new_integer(used,numstr);
 			    InputFiles i = inputfile[num];
 			    String tlemma,result;
-			    Random randomNum = new Random();
+			    
 			    int res = randomNum.nextInt(2);
 			    boolean random = (res == 1);
 			    if (random)
@@ -129,9 +195,10 @@ public class ExperimentServlet implements IServlet {
 					cargo.put("slemma", i.source_lemma);
 					cargo.put("ttheorem", i.target_theorem);
 					cargo.put("tlemma", tlemma);
-					cargo.put("stheory", i.source_location);
-					cargo.put("ttheory", i.target_location);
+					cargo.put("stheory", i.source_theory);
+					cargo.put("ttheory", i.target_theory);
 				    cargo.put("result", result);
+				    cargo.put("file_id",i.file_id);
 				
 				JsonResponse jr = new JsonResponse(webRequest, cargo);
 				WebUtils2.sendJson(jr, webRequest);
@@ -144,8 +211,38 @@ public class ExperimentServlet implements IServlet {
 			}
 			if (webRequest.actionIs("save"))
 			{
-				 //webRequest.get("their_suggestion");
-				
+				 String timestamp = webRequest.get("timestamp");
+				 String result = webRequest.get("result");
+				 String file_id = webRequest.get("file_id");
+				 String their_suggestion = webRequest.get("their_suggestion");
+				 String correctness = webRequest.get("correctness");
+				 String proveability = webRequest.get("proveability");
+				 String amended_suggestion = webRequest.get("amended_suggestion");
+				 String helpful = webRequest.get("helpful");
+				 
+				 String all = "file_id = "+file_id+"\nresult = "+result+"\ntheir_suggestion = "+
+						 "\ncorrectness = "+correctness+"\nproveability = "+proveability+"\namdended_suggestion = "+
+						 amended_suggestion + "\nhelpul = "+helpful+"\n\n\n------------------------\n\n\n";
+				 
+				 try {
+					 
+					    File l = FileUtils.getWorkingDirectory();
+						String path = l.getAbsolutePath();
+						
+						File m = new File(path+"/web/static/experiment_results/"+timestamp);
+						if (!m.exists())
+						{
+							m.createNewFile();
+						}
+						
+						
+					    Files.write(Paths.get( path+"/web/static/experiment_results/"+timestamp), all.getBytes(), StandardOpenOption.APPEND);
+					}catch (IOException e) {
+						e.printStackTrace();
+						
+					}
+				 
+				 
 			}
 		}
 	}
--- simple-server/web/static/experiment/blendui.js
+++ simple-server/web/static/experiment/blendui.js
@@ -3,6 +3,9 @@
 var num = 0;
 var result = true;
 var currentname = '';
+var file_ids = "";
+var file_id = "";
+var tlemma = "";
 
 
 $(function() {
@@ -21,7 +24,7 @@ $(function() {
 		
 	
 
-        load_st(num);
+        load_init();
 
 
 		$('#l_their_suggestion').hide();
@@ -44,10 +47,7 @@ $(function() {
 		
 		$('.doNext').on('click',function(e){
 			e.preventDefault();
-			if (!callNext())
-			{
-				return false;
-			}
+			callNext();
 
 		});
 		
@@ -57,10 +57,32 @@ $(function() {
 
 function st()
 {
-  $('textarea[name=tlemma]').val(currentname);
+  $('textarea[name=tlemma]').val(tlemma);
 
 }
 
+
+function load_init()
+{
+$.ajax({
+	url: '/cmd/Experiment',
+	data: {
+		action:"example"
+			}
+        })
+	.then(function(c,d)
+	{
+
+		console.warn(c,d);
+	    
+	    $('textarea[name=slemma]').val(c.cargo.slemma);
+	    $('textarea[name=ttheorem]').val(c.cargo.ttheorem);
+	    $('textarea[name=stheorem]').val(c.cargo.stheorem);
+	    $('textarea[name=stheory]').val(c.cargo.stheory);
+	    $('textarea[name=ttheory]').val(c.cargo.ttheory);
+	    tlemma = c.cargo.tlemma;
+	});
+}
 
 function save_st()
 {
@@ -70,7 +92,9 @@ $.ajax({
 	url: '/cmd/experiment',
 	data: {
 			action:"save",
+			timestamp:currentname,
 	        result:res,
+	        file_id:file_id,
 	    	suggestion:$('#their_suggestion').val(),
 	    	correctness:$('#correctness').val(),
 	    	proveability:$('#proveability').val(),
@@ -86,7 +110,7 @@ function load_st(num)  {
 	url: '/cmd/Experiment',
 	data: {
 		action:"get",
-		current:(num.toString())
+		used:file_ids
 			}
         })
 	.then(function(c,d)
@@ -99,7 +123,13 @@ function load_st(num)  {
 	    $('textarea[name=stheorem]').val(c.cargo.stheorem);
 	    $('textarea[name=stheory]').val(c.cargo.stheory);
 	    $('textarea[name=ttheory]').val(c.cargo.ttheory);
-	    currentname=c.cargo.tlemma;
+	    tlemma = c.cargo.tlemma;
+	    file_id = c.cargo.file_id;
+	    if (file_ids == "")
+	    	{file_ids = file_id;}
+	    else
+	    	{file_ids = file_ids+" "+file_id;}
+	    //file_ids = file_ids.substring(0,(file_ids.length-1));
 		result = (c.cargo.result =="yes");
 	   });		
     
@@ -117,31 +147,40 @@ function getInput(name) {
 
 function callNext()
 {
-    if (num==1)
+    if (num==5)
     {
     	//Go to finished page....
-    	window.event.returnValue = false;
-    	window.location.href='localhost:8300/static/experiment/thanks.html';
-    	window.alert("Thanks for participating!");
+    	//window.event.returnValue = false;
+    	
+    	//window.alert("Thanks for participating!");
+    	save_st();
+    	window.location.href='http://localhost:8300/static/experiment/thanks.html';
         return false;
     }
     else{
-    	save_st();
+    	if (num != 0)
+    	{save_st();}
 		$('textarea[name=tlemma]').val('');  
-		num = num +1;
+		
 		load_st(num);
+		num = num + 1;
 		$('#l_their_suggestion').hide();
 		$('#their_suggestion').hide();	
+		$('#their_suggestion').val('');
 		$('#l_correctness').hide();
 		$('#correctness').hide();	
+		$('#correctness').val('');
 		$('#l_proveability').hide();
 		$('#proveability').hide();	
-         	$('#l_amended_suggestion').hide();
+		$('#proveability').val('');	
+        $('#l_amended_suggestion').hide();
 		$('#amended_suggestion').hide();
+		$('#amended_suggestion').val('');
 		$('#l_helpful').hide();
 		$('#helpful').hide();	
+		$('#helpful').val('');
 		$('#nextbtn').hide();	
-		return true;b
+		return true;
 	}
 
 }
