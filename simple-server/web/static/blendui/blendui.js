55dd74286eebc134c7f92bbc865cbd02def7a03e
Ewen Maclean <ewenmaclean@gmail.com>
2016-06-20 21:24:25 +0100

    a

--- simple-server/web/static/blendui/blendui.js
+++ simple-server/web/static/blendui/blendui.js
@@ -182,7 +182,7 @@ function callBackend(action,index) {
 			console.warn(c,d);
 			$('.outputLoading').hide();
 			$('textarea[name=output]').removeClass('loading'); 
-			$('textarea[name=output]').val(c.cargo.theory);	
+			$('textarea[name=output]').val(c.cargo.theory+"\n\n"+c.cargo.blend);	
 			$('textarea[name=blendtheory]').val(c.cargo.blend);
 			$('iframe[id=svg]').attr("src",c.cargo.svg);
 			});
